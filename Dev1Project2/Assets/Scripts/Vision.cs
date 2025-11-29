69d172cbed58e33f53399423b9c39f33a452ff10
Nick Ronald <smartkidnick@icloud.com>
2025-11-29 01:34:27 -0500

    Cleanup and final Gunner

--- Dev1Project2/Assets/Scripts/Vision.cs
+++ Dev1Project2/Assets/Scripts/Vision.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class Vision : MonoBehaviour
 {
     public bool sees_player = false;
-    private GameObject entity;
+    private bool player_in_field = false;
+    private Transform entity;
     private GameObject player;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            entity = gameObject;
+            entity = transform.parent;
             player = other.gameObject;
 
+            player_in_field = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            player_in_field = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (player_in_field)
+        {
             Vector3 direction = new Vector3(player.transform.position.x - entity.transform.position.x, player.transform.position.y - entity.transform.position.y, 0);
             direction = direction.normalized;
             Debug.DrawRay(entity.transform.position, direction * 15f, Color.yellow);
             if (Physics.Raycast(entity.transform.position, direction, out RaycastHit hitInfo, 100f, LayerMask.GetMask("Player", "Default")))
             {
-                if(hitInfo.collider.tag == "Player")
+                if (hitInfo.collider.tag == "Player")
                 {
                     sees_player = true;
                 }
@@ -34,7 +52,6 @@ public class Vision : MonoBehaviour
         {
             sees_player = false;
         }
-        
     }
 
 }
